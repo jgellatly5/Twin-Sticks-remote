6178e24a738a8d196bacc70b867c2b47009e9c71
jgellatly5 <jgellatly5@gmail.com>
2016-08-01 23:16:11 -0700

    first commit
    first commit

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+
+    private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+        GameObject player = GameObject.FindWithTag("Player");
+        playerTransform = player.transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(playerTransform);
+    }
+}
